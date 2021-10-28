713407aaef2949fedc698263a3fbe2d60f0b9792
JoynoolAbedinMithoon <mithun55@gmail.com>
2021-10-28 13:09:37 +0600

    database added

--- index.js
+++ index.js
@@ -9,10 +9,23 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
 async function run ()
 {
     try {
+        await client.connect();
+        const database = client.db('test');
+        const usersCollection = database.collection('users');
 
+        //GET API
+        app.get('/users', async (req, res) =>
+        {
+            const cursor = usersCollection.find({});
+            const users = await cursor.toArray();
+            res.send(users);
+        });
     }
     finally {
         // await client.close();
